167c8cf4d145be850ecf44585aa8d917eae3a7f0
san <s.schmerek@gmail.com>
2024-10-13 21:03:50 +0200

    first things

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+ import '../app.scss';
+
+ const currentYear = new Date().getFullYear();
+</script>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p>&copy; {currentYear} S.Schmerek</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style lang="scss">
+h1 {
+    font-size: 2em;
+    text-align: center;
+    line-height: 10em;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > div {
+        display: grid;
+        width: 300px;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 1em;
+
+        > div {
+            background: linear-gradient(10deg, black, red);
+        }
+    }
+}
+</style>
+<h1>libberflabber Wobblewob</h1>
+
+<div class="container">
+    <div class="panel">
+        <div>
+            <span>Coding</span>
+        </div>
+        <div>
+            <span>Coding</span>
+        </div>
+        <div>
+            <span>Coding</span>
+        </div>
+        <div>
+            <span>Coding</span>
+        </div>
+        <div>
+            <span>Coding</span>
+        </div>
+    </div>
+</div>
