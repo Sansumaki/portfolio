4f42fc2dc8793cad42392744d915adb340f5a935
SSchmerek <s@schmerek.me>
2024-10-14 20:33:40 +0200

    add fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,17 @@
  const currentYear = new Date().getFullYear();
 </script>
 
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#about">Abowewwut</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
 <main>
     <slot />
 </main>
