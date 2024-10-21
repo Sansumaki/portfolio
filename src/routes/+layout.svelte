690525ddcc44d75d866eed9a7839f468c7075935
SSchmerek <s@schmerek.me>
2024-10-21 20:33:39 +0200

    update logo and logo logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import '../app.scss';
+	import Logo from '$lib/logo.svelte';
+import '../app.scss';
 
 	const currentYear = new Date().getFullYear();
 	let headerHeight: number;
@@ -8,34 +9,27 @@
 </script>
 
 <header class="header" bind:clientHeight={headerHeight}>
-	<div>
+	<div class="header-container">
 		<div>
 			<div class="left">
-				<a href="/"> <img src="/SanLogo.svg" alt="Logo" class="logo" /></a>
+				<a href="/">
+                    <Logo height="60px"/>
+                </a>
 			</div>
 			<div class="right">
 				<nav>
 					<div>
 						<ul class="prim-menu">
-							<li><a href="#home">Home</a></li>
-							<li><a href="#about">About</a></li>
-							<li><a href="#projects">Projects</a></li>
-							<li><a href="#contact">Contact</a></li>
+							<li><a href="#home"><span>// home</span></a></li>
+							<li><a href="#about"><span>// about</span></a></li>
+							<li><a href="#projects"><span>// projects</span></a></li>
+							<li><a href="#contact"><span>// contact</span></a></li>
 						</ul>
 					</div>
 				</nav>
 			</div>
 		</div>
 	</div>
-	<nav>
-		<ul>
-			<li><a href="#home">Home</a></li>
-			<li><a href="#about">About</a></li>
-			<li><a href="#projects">Projects</a></li>
-			<li><a href="#contact">Contact</a></li>
-		</ul>
-	</nav>
-	<img src="/SanLogo.svg" alt="Logo" class="logo" />
 </header>
 
 <main style="--main-top: {mainTop}">
@@ -50,15 +44,14 @@
 	@import '../app.scss';
 
 	header {
-		background-color: black;
-		color: white;
-		padding: 20px 0;
-		text-align: center;
-		position: sticky;
+		vertical-align: middle;
+		position: absolute;
+		left: 0;
+		right: 0;
 		top: 0;
 		z-index: 100;
 
-		> div {
+		.header-container {
 			padding: 0 4.4vh;
 			position: relative;
 			z-index: 1;
@@ -66,40 +59,40 @@
 
 			> div {
 				height: 100%;
-				-webkit-box-pack: justify;
-				-webkit-justify-content: space-between;
-				-ms-flex-pack: justify;
-				justify-content: space-between;
+				@include justify-between;
 				@include display-flex;
 				@include align-items(center);
 
 				.left {
 					@include display-flex;
 					@include align-items(center);
+
+					a {
+						margin: 0;
+					}
+					.logo {
+						fill: red;
+                        color: red;
+						height: 60px;
+						margin: 0;
+					}
 				}
 
 				.right {
-					-webkit-box-pack: end;
-					-webkit-justify-content: flex-end;
-					-ms-flex-pack: end;
-					justify-content: flex-end;
+					@include justify-end;
 					@include display-flex;
 					@include align-items(center);
 					float: right;
 
 					nav {
-						position: absolute;
-						left: 50%;
-						-webkit-transform: translateX(-50%);
-						-ms-transform: translateX(-50%);
-						transform: translateX(-50%);
+						@include absolute-center;
 						cursor: default;
-						position: relative;
 						z-index: 10;
 						font-weight: 500;
 						height: 100%;
 						@include display-flex;
 						@include align-items(center);
+						display: block;
 
 						div {
 							height: 100%;
@@ -112,6 +105,32 @@
 								letter-spacing: -1px;
 								@include display-flex;
 								@include align-items(center);
+
+								li {
+									a {
+										padding: 0 1.3vw;
+										height: 100%;
+
+										span {
+											line-height: 1.3;
+											position: relative;
+
+											&:before {
+												content: '0' counter(number);
+												display: inline-block;
+												font-weight: 700;
+												width: auto;
+												right: 0;
+												top: -0.75em;
+												line-height: 0.6em;
+												font-size: 0.6em;
+												position: absolute;
+												height: auto;
+												opacity: 0.75;
+											}
+										}
+									}
+								}
 							}
 						}
 					}
@@ -146,11 +165,5 @@
 		padding: 20px 0;
 		background-color: #333;
 		color: #fff;
-	}
-
-	.logo {
-		color: white;
-		height: 80px;
-		margin-top: 2em;
 	}
 </style>
