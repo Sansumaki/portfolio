72060a4f2ca5aedf3adf1b3d2af8434fc04d5d2a
santasia <s.schmerek@gmail.com>
2025-09-20 17:06:40 +0200

    Update +page.svelte

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import Section from '$lib/section.svelte';
 	import * as m from '$lib/paraglide/messages';
-	import { PUBLIC_CONTACT_EMAIL, PUBLIC_CONTACT_PHONE, PUBLIC_CONTACT_ADDRESS } from '$env/static/public';
 	import { formatPhoneNumber } from '$lib/phoneNumberConverter';
 
 </script>
 
 <Section title="<{m.impressum()} />">
 	<h2>{m.impressum_verantwortlich()}</h2>
-	<p>{@html PUBLIC_CONTACT_ADDRESS}</p>
 	<h2>{m.contact()}</h2>
 	<p>
-		E-Mail: <a href="mailto:{PUBLIC_CONTACT_EMAIL}">{PUBLIC_CONTACT_EMAIL}</a><br />
-		Mobile: <a href="tel:{PUBLIC_CONTACT_PHONE}">{formatPhoneNumber(PUBLIC_CONTACT_PHONE)}</a>
+		E-Mail: <a href="mailto:info@it-san.dev">info@it-san.dev</a><br />
+	
 	</p>
 </Section>
